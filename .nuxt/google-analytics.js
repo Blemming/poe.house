a83a94bb34cee26b8f476f9ff64bd5e9175055f1
dnewell <dnewell@beaulieucanada.ca>
2018-12-19 15:50:08 -0500

    trying to remove it

--- .nuxt/google-analytics.js
+++
@@ -1,7 +0,0 @@
-import Vue from 'vue'
-import VueAnalytics from 'vue-analytics'
-
-export default async function ({ app: { router } }) {
-  const moduleOptions = {"track":"PageView","id":"UA-131152589-1"}
-  Vue.use(VueAnalytics, Object.assign({ router }, moduleOptions))
-}
--- .nuxt/vee-validate.js
+++
@@ -1,6 +0,0 @@
-import Vue from 'vue'
-import VeeValidate, { Validator } from 'vee-validate'
-
-Vue.use(VeeValidate, {
-  "aria": true
-})
